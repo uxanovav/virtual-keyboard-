2147b5e03bb0b77fe28b32f94358192eb005f380
uxanovav <uxanovav@yandex.ru>
2020-11-03 12:19:43 +0400

    feat: keyboard init

---
+++ scripts.js
@@ -0,0 +1,60 @@
+const inputArea = document.querySelector('#main-text-area');
+const keyboard = document.querySelector('.keyboard');
+const firstLine = document.querySelector('.first-line');
+const secondLine = document.querySelector('.second-line');
+const thirdLine = document.querySelector('.third-line');
+const fourLine = document.querySelector('.four-line');
+
+
+
+let keyboardFlag = false;
+
+inputArea.addEventListener('focus', showKeyboard);
+inputArea.addEventListener('blur', showKeyboard);
+
+
+function showKeyboard(){
+    if (keyboardFlag === false) {
+        keyboard.style.height = '50%';
+        keyboard.style.display = 'block';
+        keyboardFlag = true;
+    } else {
+        keyboard.style.height = '0';
+        keyboard.style.display = 'none';
+        keyboardFlag = false;
+    }
+}
+
+const arr1 = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61];
+const arr2 = [113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92];
+const arr3 = [97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39];
+const arr4 = [122, 120, 99, 118, 98, 110, 109, 44, 46, 47];
+
+function init() {
+    let line1 = '';
+    let line2 = '';
+    let line3 = '';
+    let line4 = '';
+
+    for (let i = 0; i < arr1.length; i++) {
+        line1 += `<div class = "key">`+ String.fromCharCode(arr1[i])+`</div>`;
+    }
+    firstLine.innerHTML = line1;
+
+    for (let i = 0; i < arr2.length; i++) {
+        line2 += `<div class = "key">`+ String.fromCharCode(arr2[i])+`</div>`;
+    }
+    secondLine.innerHTML = line2;
+
+    for (let i = 0; i < arr3.length; i++) {
+        line3 += `<div class = "key">`+ String.fromCharCode(arr3[i])+`</div>`;
+    }
+    thirdLine.innerHTML = line3;
+
+    for (let i = 0; i < arr4.length; i++) {
+        line4 += `<div class = "key">`+ String.fromCharCode(arr4[i])+`</div>`;
+    }
+    fourLine.innerHTML = line4;
+}
+
+init();
